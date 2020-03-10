70f1601849e5e9e7085361df82dbcfe214e709f8
mayacobb <62029215+mayacobb@users.noreply.github.com>
2020-03-10 10:33:01 -0700

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(400, 400);
+  background(0);
+}
+
+function draw() {
+  var r = random (0,255);
+  var g = 0;
+  var b = random(0,255);
+
+  fill(r, g, b, 100);
+  noStroke()
+  
+  var a = random(0, 400);
+  var b = random(0, 400);
+
+  ellipse(a, b, 25, 25);
+
+}
